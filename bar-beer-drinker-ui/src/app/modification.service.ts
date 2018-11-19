0cd26afad20602a9fcc8f5c4356bea6542ab96fe
samer s <samershaban11@gmail.com>
2018-11-18 22:47:50 -0500

    finished

---
+++ bar-beer-drinker-ui/src/app/modification.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Result{
+  first: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class ModificationService {
+
+  constructor(
+    public http: HttpClient
+  ) { }
+
+  getModification(modification: string) {
+    return this.http.get<any[]>('/api/modification/' + encodeURIComponent(modification));
+  }
+
+  sendQuery(query: string){
+    return this.http.get<Result>('/api/update/' + query);
+  }
+}
+
+
+
